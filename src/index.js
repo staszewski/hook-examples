787e3ece9151add53d6f15efcc1e81b5167b2237
staszewski <kamilstaszewski95@gmail.com>
2019-07-27 21:41:10 +0200

    UseReducerExample

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import UseContextExample from './UseContextExample';
 import UseRefExample from './UseRefExample'
+import UseReducerExample from './UseReducerExample'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<UseRefExample />, document.getElementById('root'));
+ReactDOM.render(<UseReducerExample/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
